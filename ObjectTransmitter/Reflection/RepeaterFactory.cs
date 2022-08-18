173e84fb6fb066dd023bdc974a6af9182491ed2f
vsoff <rucamper@mail.ru>
2022-08-18 14:49:22 +0300

    [master][added] Commit missed RepeaterFactory.cs and RepeaterInstanceFactory.cs

---
+++ ObjectTransmitter/Reflection/RepeaterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObjectTransmitter.Reflection
+{
+    public abstract class RepeaterFactory
+    {
+        public abstract Type RepeaterType { get; }
+        public abstract object Create();
+    }
+
+    public abstract class RepeaterFactory<T> : RepeaterFactory
+    {
+        public override Type RepeaterType { get; } = typeof(T);
+
+        public abstract T CreateRepeater();
+
+        public override object Create() => CreateRepeater();
+    }
+}
---
+++ ObjectTransmitter/Reflection/RepeaterInstanceFactory.cs
@@ -0,0 +1,27 @@
+﻿using ObjectTransmitter.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectTransmitter.Reflection
+{
+    internal class RepeaterInstanceFactory
+    {
+        private readonly IReadOnlyDictionary<Type, RepeaterFactory> _repeaterFactoriesByType;
+
+        public RepeaterInstanceFactory(IEnumerable<RepeaterFactory> repeaterFactories)
+        {
+            if (repeaterFactories == null) throw new ArgumentNullException(nameof(repeaterFactories));
+
+            _repeaterFactoriesByType = repeaterFactories.ToDictionary(x => x.RepeaterType, x => x);
+        }
+
+        public object CreateInstance(Type type)
+        {
+            if (!_repeaterFactoriesByType.TryGetValue(type, out var repeaterFactory))
+                throw new ObjectTransmitterException($"Repeater factory for type `{type.FullName}` is not registered");
+
+            return repeaterFactory.Create();
+        }
+    }
+}
